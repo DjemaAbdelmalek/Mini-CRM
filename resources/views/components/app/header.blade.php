ca2612cad27bbd5d933a16632b5a6b654d421450
Malek Djema <mr.djemaabdelmalek@gmail.com>
2025-04-30 14:16:46 +0100

    feat: clients is started v2

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -1,9 +1,37 @@
-<nav class="fixed inset-0 top-0 flex items-center justify-between px-4 transition-all duration-300 shadow-lg h-14 bg-bg-300 dark:shadow-bg-300 dark:shadow-sm"
+<nav class="fixed inset-0 top-0 z-50 flex items-center justify-between px-4 transition-all duration-300 shadow-lg h-14 bg-bg-300 dark:shadow-bg-300 dark:shadow-sm"
     :class="expanded ? 'md:left-[256px] left-100' : 'left-[64px]'">
     <h1 class="text-2xl font-semibold">{{ $titleOfPage }}</h1>
-    <div class="items-center justify-between hidden md:flex">
-        <h1>User</h1>
-        <div>Drop down of that page</div>
+    <div class="items-center justify-between hidden space-x-10 md:flex">
+        @if (request()->is('/'))
+            <div x-data="{ menu: false }" class="relative w-40 border-2 rounded-lg cursor-pointer border-bg-200">
+                <div class="flex items-center justify-center h-8 space-x-2" @mouseenter="menu = true"
+                    @mouseleave="menu = false">
+                    <i class=" fas fa-home"></i>
+                    <span>Accueil</span>
+                </div>
+                <div x-show="menu" @mouseenter="menu = true" @mouseleave="menu = false"
+                    class="absolute left-0 flex flex-col w-full space-y-2 border-2 bg-bg-200 top-8 border-bg-200">
+                    <a href="/" class="w-full"> Accueil </a>
+                </div>
+            </div>
+        @endif
+        @if (request()->is(['zones', 'clients', 'roles', 'users']))
+            <div x-data="{ menu: false }" class="relative w-40 border-2 rounded-lg cursor-pointer border-bg-200">
+                <div class="flex items-center justify-center h-8 space-x-2" @mouseenter="menu = true"
+                    @mouseleave="menu = false">
+                    <i class=" fas fa-cog"></i>
+                    <span>Admin</span>
+                </div>
+                <div x-show="menu" @mouseenter="menu = true" @mouseleave="menu = false"
+                    class="absolute left-0 flex flex-col w-full space-y-2 border-2 shadow-lg bg-bg-200 top-8 border-bg-200">
+                    <a href={{ route('zones') }} class="block w-full px-2 py-1 pl-6 bg-bg-200 hover:bg-bg-300">Zones</a>
+                    <a href={{ route('roles') }} class="block w-full px-2 py-1 pl-6 bg-bg-200 hover:bg-bg-300">Roles</a>
+                    <a href={{ route('users') }} class="block w-full px-2 py-1 pl-6 bg-bg-200 hover:bg-bg-300">Users</a>
+                    <a href={{ route('clients') }}
+                        class="block w-full px-2 py-1 pl-6 bg-bg-200 hover:bg-bg-300">Clients</a>
+                </div>
+            </div>
+        @endif
         <a href="#" @click.prevent="toggleDarkMode()" x-text="darkMode ? '🔆' : '🌑'" class="text-2xl"></a>
     </div>
 </nav>
--- resources/views/components/sidebar/section.blade.php
+++ resources/views/components/sidebar/section.blade.php
@@ -1,7 +1,7 @@
 @props(['icon', 'title'])
 
 <div x-data="{ open: false }" class="w-full">
-    <div @click="open = !open" class="flex items-center px-4 py-3 cursor-pointer space-x-2 w-full hover:bg-bg-200"
+    <div @click="open = !open" class="flex items-center w-full px-4 py-3 space-x-2 cursor-pointer hover:bg-bg-200"
         :class="open ? 'bg-bg-200' : ''">
         <i class="{{ $icon }}"></i>
         <span x-show="expanded" class="transition-opacity duration-300 whitespace-nowrap">
@@ -9,13 +9,13 @@
         <svg x-show="open" class="w-4 h-4 ml-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
         </svg>
-        <svg x-show="!open && expanded" class="w-4 h-4 rotate-180 ml-auto" fill="none" stroke="currentColor"
+        <svg x-show="!open && expanded" class="w-4 h-4 ml-auto rotate-180" fill="none" stroke="currentColor"
             viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
         </svg>
     </div>
     <div x-show="openSectionSideBar && open" x-transition
-        class="space-y-1 transition-opacity duration-300 w-full bg-bg-200">
+        class="w-full space-y-1 transition-opacity duration-300 bg-bg-200">
         {{ $slot }}
     </div>
 </div>
